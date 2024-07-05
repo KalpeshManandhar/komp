6f9f33513c77bc6924b06664849c936a512be194
kalpesh <manandhar.kalpesh@gmail.com>
2024-07-05 22:16:51 +0545

    bugfix: splice compare with literal also takes length into account now
    previously, only compared upto the smaller length

--- src/tokenizer/str.h
+++ src/tokenizer/str.h
@@ -19,8 +19,9 @@ static int copyToArr(Splice src, char *dest, int destSize){
 }
 
 static bool compare(Splice s, const char* str){
-    int last = std::min(strlen(str), s.len);
-    return strncmp(s.data, str, last) == 0;
+    if (strlen(str) != s.len) 
+        return false; 
+    return strncmp(s.data, str, s.len) == 0;
 }
 
 static std::ostream & operator <<(std::ostream &out, Splice s){
