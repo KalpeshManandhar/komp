2b234749bf60bf2daff3ef875394ca4cb687cf17
kalpesh <manandhar.kalpesh@gmail.com>
2024-10-23 14:25:37 +0545

    Oopsie: Deleted accidentally added path info file.
    Removed the path info file which contains the paths for the RV64 toolchain and emulator, which is hence device specific.
    Added instructions in the run_tests.ps1 file about the path info needed

--- tests/codeGen/run_tests.ps1
+++ tests/codeGen/run_tests.ps1
@@ -5,8 +5,18 @@ Param(
 
 $test_folder = ".\tests\codeGen"
 
-# add the following things to this file
+
+# add the following things to a new path_info.ps1 file
+# # the riscv toolchain gcc executable in linux
+# $gcc_toolchain = 
+# $riscv_gcc = 
+# $qemu = 
+# $sysroot = 
+
+# # path for current wd from linux
+# $cwd_from_linux = 
 . "$test_folder/path_info.ps1"
+
 
 . "$test_folder/expected_info.ps1"
 $found_values = $expected_values.Clone()
