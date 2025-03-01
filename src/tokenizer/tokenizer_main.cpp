b622dd8c254478f2caf18f414910b50aabf5f649
Ishwor Basyal <ishwor8basyal@gmail.com>
2025-03-01 19:20:37 +0545

    Output formatted tokenizer
    Generates an md file of all tokens

--- src/tokenizer/tokenizer_main.cpp
+++ src/tokenizer/tokenizer_main.cpp
@@ -13,14 +13,25 @@ int main(int argc, char ** argv){
     t.init();
     t.loadFileToBuffer(argv[1]);
 
-    while (true){
+    // Output to md file
+    std::ofstream mdFile("token_output.md");
+    mdFile << "| Token Value | Token Type |\n";
+    mdFile << "|-------------|------------|\n";
+
+    while (true) {
         Token token = t.nextToken();
+        // std out write
         std::cout<<token.string<<":\t"<<TOKEN_TYPE_STRING[token.type]<<"\n";
-        if (token.type == TOKEN_EOF){
+
+        // md file write
+        mdFile << "| " << std::string(token.string.data, token.string.len) << " | " << TOKEN_TYPE_STRING[token.type] << " |\n";
+
+        if (token.type == TOKEN_EOF) {
             break;
         }
     }
 
+    mdFile.close();
     return 0;
     
     
